272f9039a30071f16fec14d21ae46bdd5dc3c404
JasonDollar <elrey33@gmail.com>
2019-11-30 13:09:26 +0100

    got rid off of loading spinner

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -32,7 +32,7 @@ const Layout = ({ children }) => {
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
           crossOrigin="anonymous"
         /> */}
-        <link rel="stylesheet" href="/nprogress.css" type="text/css" />
+        <link rel="stylesheet" href="/nprogress.css" />
       </Head>
       <TopMenu />
       <Container>
